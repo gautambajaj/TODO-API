299146ec2e84ac66731a98a3c84dde679aebe189
Gautam Bajaj <gautam.bajaj@uwaterloo.ca>
2018-01-14 13:02:12 -0500

    CRUD functionality added

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require('express');
 // middleware body-parser extracts the entire body portion of an incoming request stream and exposes it on request.body
 var bodyParser = require('body-parser')
 var server = express();
+var _ = require('underscore');
 var PORT = process.env.PORT || 3000;
 
 var todoCollection = [];
@@ -20,7 +21,7 @@ server.get('/todos', function(request,response){
 
 // GET todo by ID
 server.get('/todos/:id', function(request,response){
-	var requiredID = request.params.id;
+	var requiredID = parseInt(request.params.id,10);
 	var requiredTodo;
 
 	todoCollection.forEach(function(todo){
@@ -36,14 +37,74 @@ server.get('/todos/:id', function(request,response){
 	}
 });
 
+
+// delete todo by ID
+server.delete('/todos/:id', function(request,response){
+	var requiredID = parseInt(request.params.id,10);
+	var requiredTodo;
+
+	todoCollection.forEach(function(todo){
+		if(requiredID === todo.id){
+			requiredTodo = todo;
+		}
+	});
+
+	if(requiredTodo){
+		todoCollection = _.without(todoCollection,requiredTodo);
+		response.json(requiredTodo);
+	} else {
+		response.status(404).send();
+	}
+});
+
+
 // POST todo
 server.post('/todos', function(request,response){
-	var body = request.body;
+	var body = _.pick(request.body, 'description', 'completed');
+
+	if(!_.isString(body.description) || !_.isBoolean(body.completed)){
+		return response.status(400).send(); // bad request
+	}
+
 	body.id = nextTodoID;
 	++nextTodoID;
 
 	todoCollection.push(body);
 	response.json(body);
+});
+
+// PUT todo by ID
+server.put('/todos/:id', function(request,response){
+	var body = _.pick(request.body, 'description', 'completed');
+
+	var requiredID = parseInt(request.params.id,10);
+	var requiredTodo;
+	todoCollection.forEach(function(todo){
+		if(requiredID === todo.id){
+			requiredTodo = todo;
+		}
+	});
+
+	if(!requiredTodo){
+		return response.status(404).send();
+	}
+
+	var putAttributes = {};
+
+	if(body.hasOwnProperty('description') && _.isString(body.description)){
+		putAttributes.description = body.description;
+	} else if(body.hasOwnProperty('description')){
+		return response.status(400).send();
+	}
+
+	if(body.hasOwnProperty('completed') && _.isBoolean(body.completed)){
+		putAttributes.completed = body.completed; 
+	} else if(body.hasOwnProperty('completed')){
+		return response.status(400).send();
+	}
+
+	_.extend(requiredTodo,putAttributes);
+	response.json(requiredTodo);
 });
 
 server.listen(PORT, function(){
